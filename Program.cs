b80c210cb6e43446a684490904f6b65c06a9012c
Daniil Levin <dan3000pro@mail.ru>
2024-01-29 01:24:02 +0400

    added stalker3, added new action Yell()

--- Program.cs
+++ Program.cs
@@ -20,9 +20,15 @@ class Program
         Stalker stalker_2 = new Stalker("Игорь Ипучий", 145, 1, false);
         stalker_2.PlayGuitar();
 
+        Stalker stalker_3 = new Stalker("Ляля Афган", 150, 2, false);
+        stalker_3.Yell("Свободовцы все петухи!");
+
+        Stalker stalker_4 = new Stalker("Максимка Чушпан", 300, 40, false);
+        stalker_4.PlayFootball(40);
+
+        Stalker stalker_5 = new Stalker("Говноед Медведыч", 200, 40, false);
+        stalker_5.RIP();
         
         Console.ReadLine();
     }
-
-
 }
--- Stalker.cs
+++ Stalker.cs
@@ -8,20 +8,20 @@
     {
     public class Stalker
     {
-            public string Name = "Дуля Данилов";
-            public int Hp; // 0 1 2 3 -4 -5 
-            public float Speed; //0.2341231241243
-            public bool dead; //true или false
+        public string Name = "Дуля Данилов";
+        public int Hp; // 0 1 2 3 -4 -5 
+        public float Speed; //0.2341231241243
+        public bool dead; //true или false
 
         public Stalker Friend;
 
-            public Stalker(string name, int hp, float speed, bool dead)
-            {
-                Name = name;
-                Hp = hp;
-                Speed = speed;
-                this.dead = dead;
-            }
+        public Stalker(string name, int hp, float speed, bool dead)
+        {
+            Name = name;
+            Hp = hp;
+            Speed = speed;
+            this.dead = dead;
+        }
 
         public void EatTushonka(int Eat)
         {
@@ -29,10 +29,7 @@
             Console.WriteLine(Name + " поел тушенки и пополнил " + Eat + " здоровья ");
             Console.WriteLine($"У {Name} теперь {Hp} здороыья");
         }
-        public void RunFromTushkan()
-        {
-            Console.WriteLine(Name + " убежал от тушкана со скоростью " + Speed);
-        }
+        public void RunFromTushkan() => Console.WriteLine(Name + " убежал от тушкана со скоростью " + Speed);
 
         public void RecieveDamage(int damage)
         {
@@ -42,11 +39,19 @@
             Console.WriteLine($"У {Name} теперь {Hp} здоровья");
         }
 
-        public void PlayGuitar()
+        public void PlayGuitar() => Console.WriteLine(Name + " Играет на гитаре");
+        public void Yell(string phrase) => Console.WriteLine($"{Name} кричит {phrase}");
+        public void PlayFootball(float speed)
         {
-            Console.WriteLine(Name + " Играет на гитаре");
+            Console.WriteLine(Name + " уебал по мячу со скоростью " + speed + " км/ч");
         }
+        public void RIP()
+        {
+            Console.WriteLine(Name + " стоял на воротах и умер");
 
+        }
+        
+      
     }
 
 }
