f08bca46dd9569be39960d6e9fa9b0e739ab11de
DulyaDanilov <dulinov01@mail.ru>
2024-01-29 01:02:37 +0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Training;
+
+class Program
+
+{
+
+
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Дуля Данилов");
+        Stalker stalker_1 = new Stalker("Дуля Данилов", 100, 4, false);
+        stalker_1.EatTushonka(5);
+        stalker_1.RunFromTushkan();
+        stalker_1.RecieveDamage(5);
+        Stalker stalker_2 = new Stalker("Игорь Ипучий", 145, 1, false);
+        stalker_2.PlayGuitar();
+
+        
+        Console.ReadLine();
+    }
+
+
+}
---
+++ Stalker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+
+    namespace Training
+
+
+    {
+    public class Stalker
+    {
+            public string Name = "Дуля Данилов";
+            public int Hp; // 0 1 2 3 -4 -5 
+            public float Speed; //0.2341231241243
+            public bool dead; //true или false
+
+        public Stalker Friend;
+
+            public Stalker(string name, int hp, float speed, bool dead)
+            {
+                Name = name;
+                Hp = hp;
+                Speed = speed;
+                this.dead = dead;
+            }
+
+        public void EatTushonka(int Eat)
+        {
+            Hp += Eat;
+            Console.WriteLine(Name + " поел тушенки и пополнил " + Eat + " здоровья ");
+            Console.WriteLine($"У {Name} теперь {Hp} здороыья");
+        }
+        public void RunFromTushkan()
+        {
+            Console.WriteLine(Name + " убежал от тушкана со скоростью " + Speed);
+        }
+
+        public void RecieveDamage(int damage)
+        {
+            Hp -= damage;
+
+            Console.WriteLine(Name + " получил " + damage + " урона!");
+            Console.WriteLine($"У {Name} теперь {Hp} здоровья");
+        }
+
+        public void PlayGuitar()
+        {
+            Console.WriteLine(Name + " Играет на гитаре");
+        }
+
+    }
+
+}
+    
+  
