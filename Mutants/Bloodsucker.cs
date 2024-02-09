f022d4b8a2649977a0f9ddfe9a7b46a174c03533
DulyaDanilov <dulinov01@mail.ru>
2024-02-09 16:29:35 +0400

    minor fix

---
+++ Mutants/Bloodsucker.cs
@@ -0,0 +1,7 @@
+﻿public class Bloodsucker : Mutant
+{
+    private const string _name = "Тушкан";
+    public Bloodsucker(int damage, int Hp, bool Dead) : base(damage, Hp, _name, Dead)
+    {
+    }
+}
---
+++ Mutants/Fleshe.cs
@@ -0,0 +1,8 @@
+﻿public class Flesh : Mutant
+{
+    private const string _name = "Плоть";
+    public Flesh(int damage, int Hp, bool Dead) : base(damage, Hp, _name, Dead)
+    {
+    }
+
+}
