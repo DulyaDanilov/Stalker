bf46915979269b53b912ce0fc48ee8e50b54f268
DulyaDanilov <157992811+DulyaDanilov@users.noreply.github.com>
2024-06-06 18:32:40 +0300

    added NIXUYA

---
+++ Factories/TuskanFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Factories
+{
+    public class TushkanFactory
+    {
+        private string[] _tuskan =
+            "Тушкан",
+    };
+    private Random _random = new Random();
+    public Tushkan Get()
+    {
+        Tushkan tushkan;
+        int nameindex = ("Тушкан");
+        string name = _tushkan [ nameindex];
+
+    }
+}
